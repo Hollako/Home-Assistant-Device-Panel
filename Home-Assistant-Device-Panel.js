594c5f78d9a79d926f9412d58e3b0de04d6d109c
SmartWay Systems <hollako75@hotmail.com>
2026-06-02 16:16:59 +0300

    Update version number to 2.8.1

--- Home-Assistant-Device-Panel.js
+++ Home-Assistant-Device-Panel.js
@@ -1,4 +1,4 @@
-const VERSION = "1.1.17";
+const VERSION = "2.7.1";
 class OfflineDevicePanel extends HTMLElement {
   static getConfigElement() {
     return document.createElement("offline-device-panel-editor");
